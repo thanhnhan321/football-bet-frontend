623b4e0323478f0de9c48b05c322c3df950b039e
thanhnhan321 <thanhnhan13a@gmail.com>
2026-03-06 12:24:12 +0700

    Add menu tab for season manage

--- src/pages/SeasonsPage/SeasonsPage.jsx
+++ src/pages/SeasonsPage/SeasonsPage.jsx
@@ -1,5 +1,18 @@
+import { SEASON_MENU_ITEMS } from "./constants";
+import useAdminHomePageState from "../../hooks/useAdminHomePageState";
+import MenuTabs from "../../components/blocks/MenuTabs";
+
 function SeasonsPage() {
-  return null;
+  const { activeMenu, setActiveMenu } = useAdminHomePageState();
+  return (
+    <div className="admin-user-page">
+      <MenuTabs
+        activeMenu={activeMenu}
+        onChange={setActiveMenu}
+        MENU_ITEMS={SEASON_MENU_ITEMS}
+      />
+    </div>
+  );
 }
 
 export default SeasonsPage;
---
+++ src/pages/SeasonsPage/constants.js
@@ -0,0 +1,5 @@
+export const SEASON_MENU_ITEMS = [
+  { key: "create", label: "Tạo mùa giải" },
+  { key: "edit", label: "Sửa mùa giải" },
+  { key: "delete", label: "Xóa mùa giải" },
+];
